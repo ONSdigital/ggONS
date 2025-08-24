10b4ee01aa5195e2779aa93a917f01a6f5007a43
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-08-24 15:37:43 +0100

    r cmd check fixes

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
 .onLoad <- function(libname, pkgname) {
   sysfonts::font_add(
     family = "Open Sans",
-    regular = system.file("fonts", "Open Sans", "OpenSans-Regular.ttf", package = "ggONS"),
-    italic = system.file("fonts", "Open Sans", "OpenSans-Italic.ttf", package = "ggONS"),
-    bold = system.file("fonts", "Open Sans", "OpenSans-Bold.ttf", package = "ggONS"),
-    bolditalic = system.file("fonts", "Open Sans", "OpenSans-BoldItalic.ttf", package = "ggONS")
+    regular = system.file("fonts", "OpenSans", "OpenSans-Regular.ttf", package = "ggONS"),
+    italic = system.file("fonts", "OpenSans", "OpenSans-Italic.ttf", package = "ggONS"),
+    bold = system.file("fonts", "OpenSans", "OpenSans-Bold.ttf", package = "ggONS"),
+    bolditalic = system.file("fonts", "OpenSans", "OpenSans-BoldItalic.ttf", package = "ggONS")
   )
   showtext::showtext_auto()
   showtext::showtext_opts(dpi = 300)
